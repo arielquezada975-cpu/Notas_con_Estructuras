1db61158c0902b4129f161dea11e72c2971dfb70
Annet Paez <annet.paez@udla.edu.ec>
2026-06-09 11:54:54 -0500

    agregar funcion max y min usando struct y punteros

--- agregar estructura Estudiante con nombre y notas.c
+++ agregar estructura Estudiante con nombre y notas.c
@@ -3,7 +3,7 @@
 
 #define ASIGNATURAS 3
 
-// Estructura que agrupa los datos de cada estudiante
+// Definicion de la estructura
 struct Estudiante {
     char nombre[50];
     float notas[ASIGNATURAS];
@@ -11,3 +11,22 @@ struct Estudiante {
     float max;
     float min;
 };
+
+void calcularMaxMin(struct Estudiante *est, int n)
+{
+    printf("\n--- MAX Y MIN POR ESTUDIANTE ---\n");
+    for (int i = 0; i < n; i++)
+    {
+        (est + i)->max = (est + i)->notas[0];
+        (est + i)->min = (est + i)->notas[0];
+        for (int j = 1; j < ASIGNATURAS; j++)
+        {
+            if ((est + i)->notas[j] > (est + i)->max)
+                (est + i)->max = (est + i)->notas[j];
+            if ((est + i)->notas[j] < (est + i)->min)
+                (est + i)->min = (est + i)->notas[j];
+        }
+        printf("%s -> Max: %.2f | Min: %.2f\n",
+               (est + i)->nombre, (est + i)->max, (est + i)->min);
+    }
+}
